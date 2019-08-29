3fcd3d29f378dbb9ae72a9db567da18e46d1d316
PCHIII <treyhelmer@gmail.com>
2019-08-29 11:01:44 -0400

    friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+var friends = [
+
+    {
+        "name":"Jessica",
+        "photo":"https://bodyartguru.com/wp-content/uploads/2017/07/Jessica-Alba.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+
+      {
+        "name":"Pamela",
+        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_vn-6u_mGKLR5xVAUepG91yKkgKr86PkMmC_8SxDcYJc7C6CD",
+        "scores":[
+            4,
+            3,
+            1,
+            1,
+            5,
+            3,
+            5,
+            2,
+            5,
+            4
+          ]
+      }
+
+      {
+        "name":"Miss Piggy",
+        "photo":"https://pbs.twimg.com/profile_images/1080855830365257728/I160ToWC.jpg",
+        "scores":[
+            1,
+            4,
+            2,
+            3,
+            3,
+            1,
+            4,
+            5,
+            3,
+            5
+          ]
+      }
+
+
+];
+
+module.export = friends;
